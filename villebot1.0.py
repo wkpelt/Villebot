d4c67fd8166bae902d25580844c10c140153678c
Wiljam Peltomaa <wkpelt@utu.fi>
2019-04-12 03:14:17 +0300

    faster

--- villebot1.0.py
+++ villebot1.0.py
@@ -11,13 +11,13 @@ def main():
     print('Press Ctrl-Alt-Del to stop the bot.')
     texts = []
     locations = []
-    choices = []
-    last = []
+    last = ''
     correct_answers = []
     wrong = []
     task = 0
     questions = 0
     assignment_name = "Tehtävä"
+    allDone = False
 
     layout = [[sg.Text('Montako kysymystä?'),
                sg.Text('', key='_OUTPUT_', text_color='blue', background_color='white', size=(17, 1))],
@@ -35,6 +35,10 @@ def main():
     window = sg.Window('VILLEbot v1.0', auto_size_text=False, default_element_size=(16, 1), keep_on_top=True).Layout(
         layout)
     window.SetIcon('favicon.ico')
+
+    def scrollDown():
+        for i in range(5):
+            pyautogui.press('pgdn')
 
     while True:
         event, values = window.Read(timeout=0)
@@ -57,35 +61,38 @@ def main():
             window.FindElement('_OUTPUT2_').Update(values['_IN2_'])
         if event == 'Start':
             restart = pyautogui.locateCenterOnScreen('restart.png')
-            while (len(correct_answers)) < questions:
-                for i in range(5):
-                    pyautogui.press('pgdn')
-                pos = None
+
+            while allDone is False:
+                scrollDown()
                 done = False
 
                 window.FindElement('_OUTPUT3_').Update(task + 1)
                 window.FindElement('_OUTPUT4_').Update(len(correct_answers))
                 window.Read(timeout=0)
 
-                while pos is None:
-                    for pos in pyautogui.locateAllOnScreen('blue3.png',grayscale=True):
-                        if not done:
+                for pos in pyautogui.locateAllOnScreen('blue3.png'):
+                    if not done:
+                        locations.append((pos[0] + 5, pos[1] + 5))
+                        if len(correct_answers) > task and len(correct_answers) != questions:
+                            pyautogui.click(locations[-1])
+                            window.Read(timeout=0)
+                            done = True
+                        else:
                             pyautogui.moveTo(pos[0] - 20, pos[1] + 5)
                             pyautogui.dragRel(700, 45, 0.2, button='left')
                             pyautogui.hotkey('ctrl', 'c')
                             copied_text = pyperclip.paste()
-                            copied_text.strip("\r\n")
-                            texts.append(copied_text)
-                            last.append(copied_text)
-                            locations.append((pos[0] + 5, pos[1] + 5))
-                            choices.append((pos[0] + 5, pos[1] + 5))
+                            texts.append(copied_text.strip("\r\n"))
+                            last = copied_text.strip("\r\n")
                             window.Read(timeout=0)
-                            if len(correct_answers) > task:
-                                for i in range(len(texts)):
-                                    if texts[i] in correct_answers[task]:
-                                        pyautogui.click(locations[i])
-                                        window.Read(timeout=0)
-                                        done = True
+                            if len(correct_answers) == questions:
+                                if correct_answers[task] == last:
+                                    pyautogui.click(locations[-1])
+                                    pyautogui.press('enter')
+                                    if task+1 == questions:
+                                        allDone = True
+                                    task += 1
+                                    done = True
                             else:
                                 for i in range(len(texts)):
                                     if texts[i] not in wrong[task]:
@@ -96,7 +103,7 @@ def main():
                     locations = []
                     window.Read(timeout=0)
 
-                while True:
+                while len(correct_answers) != questions:
                     window.Read(timeout=0)
                     if (questions == (task+1)):
                         time.sleep(0.1)
@@ -106,41 +113,42 @@ def main():
                         time.sleep(0.1)
                         pyautogui.press('enter')
 
+                    if len(correct_answers) > task:
+                            pyautogui.press('enter')
+                            task += 1
+                            print("Skip\n")
+                            break
+
                     green = pyautogui.locateOnScreen('green.png')
                     if green is not None:
                         task += 1
                         if len(correct_answers) >= task:
                             pyautogui.press('enter')
-                            for i in range(5):
-                                pyautogui.press('pgdn')
+                            scrollDown()
                             window.Read(timeout=0)
                             break
                         else:
                             print("Oikea vastaus:")
-                            print(last[-1], "\n")
-                            #pyautogui.moveTo(green[0], green[1])
-                            #pyautogui.dragRel(700, 45, 0.2, button='left')
-                            #pyautogui.hotkey('ctrl', 'c')
-                            #right_answer = pyperclip.paste()
-                            right_answer = last[-1]
+                            print(last, "\n")
+                            right_answer = last
                             correct_answers.append(right_answer.strip("\r\n"))
                             pyautogui.press('enter')
-                            for i in range(5):
-                                pyautogui.press('pgdn')
+                            scrollDown()
                             window.Read(timeout=0)
+                            if len(correct_answers) == questions:
+                                pyautogui.click(restart)
+                                task = 0
                             break
 
                     elif green is None:
                         print("Väärä vastaus:")
-                        print(last[-1], "\n")
-                        wrong[task].append(last[-1])
+                        print(last, "\n")
+                        wrong[task].append(last)
                         print("Arvattu ", len(wrong[task]), " kertaa tehtävää numero", len(correct_answers) + 1, "\n")
                         task = 0
                         pyautogui.click(restart)
-                        #pyautogui.click(pyautogui.locateCenterOnScreen('restart.png',grayscale=True))
                         window.Read(timeout=0)
-                        for i in range(5):
-                            pyautogui.press('pgdn')
+                        scrollDown()
                         break
 
             print("Oikeat vastaukset: ")
@@ -154,11 +162,11 @@ def main():
             window.Read(timeout=0)
             texts = []
             locations = []
-            choices = []
-            last = []
+            last = ''
             wrong = []
             correct_answers = []
             task = 0
+            pyautogui.alert(text='Valmis!', title='', button='OK')
     window.Close()
 
 main()
